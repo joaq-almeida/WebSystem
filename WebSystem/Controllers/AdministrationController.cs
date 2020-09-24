d6c0f98bf70370f41a284bf88b91de5394fbdf72
joaquim-almeida <joaquimrafael30@hotmail.com>
2020-09-23 22:06:19 -0300

    adicionado criação e listagem de role

---
+++ WebSystem/Controllers/AdministrationController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebSystem.ViewModels;
+
+namespace WebSystem.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var _roles = _roleManager.Roles;
+            return View(_roles);
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles", "Administration");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult EditRole(string id)
+        {
+            var role = _roleManager.FindByIdAsync(id);
+            return View(role);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(CreateRoleViewModel model)
+        {
+            var role = await  _roleManager.FindByNameAsync(model.RoleName);
+
+            if(role != null){
+
+                var result =  await _roleManager.DeleteAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles", "Administration");
+                }
+            }
+
+            return View(role);
+        }
+    }
+}
